227a6c1280dd49e8cbb88193264bef8ba79efaf7
Uli <uli@domain.invalid>
2019-02-12 02:07:20 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from PyQt5.QtWidgets import*
+from PyQt5.uic import loadUi
+
+from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
+
+import numpy as np
+import random
+
+import mplwidget
+
+class MatplotlibWidget(QMainWindow):
+
+    def __init__(self):
+
+        QMainWindow.__init__(self)
+
+        loadUi("hdscope.ui",self)
+
+        self.setWindowTitle("PyQt5 & Matplotlib HD Oscilloscope")
+
+        self.ch4.clicked.connect(self.update_graph)
+
+        self.addToolBar(NavigationToolbar(self.MplWidget.canvas, self))
+
+
+    def update_graph(self):
+
+        fs = 500
+        f = random.randint(1, 100)
+        ts = 1/fs
+        length_of_signal = 100
+        t = np.linspace(0,1,length_of_signal)
+
+        cosinus_signal = np.cos(2*np.pi*f*t)
+        sinus_signal = np.sin(2*np.pi*f*t)
+
+        self.MplWidget.canvas.axes.clear()
+        self.MplWidget.canvas.axes.plot(t, cosinus_signal)
+        self.MplWidget.canvas.axes.plot(t, sinus_signal)
+        self.MplWidget.canvas.axes.legend(('cosinus', 'sinus'),loc='upper right')
+        self.MplWidget.canvas.axes.set_title('Cosinus - Sinus Signal')
+        self.MplWidget.canvas.draw()
+
+
+app = QApplication([])
+window = MatplotlibWidget()
+window.show()
+app.exec_()
